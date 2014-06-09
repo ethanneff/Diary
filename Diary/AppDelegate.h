46cea422254da03e494fbba0fca7de7f15fdcb6d
Ethan Neff <eneff@us-aus-wireless-10-0-140-59.amer.corp.natinst.com>
2014-06-09 10:09:53 -0500

    Initial Commit

---
+++ Diary/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Diary
+//
+//  Created by Ethan Neff on 6/9/14.
+//  Copyright (c) 2014 Ethan Neff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
